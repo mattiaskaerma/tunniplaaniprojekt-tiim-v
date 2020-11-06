ae66d1a749367a078b9022263510da8d39c9a1c5
fs-s <55434294+fs-s@users.noreply.github.com>
2020-11-06 09:35:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,9 +31,7 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/weeks/' + currentWeek).then(respo
         <button value="2020-10-27T00:00:00Z" class="tuesday">T</button>
         <button value="2020-10-28T00:00:00Z" onclick="location.href='index2.html'" class="wednesday">K</button>
         <button value="2020-10-29T00:00:00Z" onclick="location.href='index.html'" class="thursday">N</button>
-        <button onclick=something() value="2020-10-30T00:00:00Z" onclick="location.href='index4.html'" class="friday">R</button>
-        <button value="2020-10-31T00:00:00Z" onclick="location.href='index3.html'" class="saturday">L</button>
-        <button value="2020-10-01T00:00:00Z" class="sunday">P</button>`
+        <button onclick=something() value="2020-10-30T00:00:00Z" onclick="location.href='index4.html'" class="friday">R</button>`
     container.appendChild(newBox)
 })
 
@@ -49,16 +47,14 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=' + groupId + '&we
             let newLesson = document.createElement('div')
             newLesson.classList.add("tund")
             newLesson.innerHTML = `<div class="leftside">
-        <span class="aeg">${el.timeStart}<br>${el.timeEnd}</span>
-        <br>
-    <span class="tunninr">${el.date}</span>
-    </div>
-    <div class="rightside">
+        <span class="aeg">${el.timeStart} - ${el.timeEnd}</span>
         <span class="pealkiri">${el.nameEn}</span>
-        <span class="ruum">${el.rooms[0].roomCode}</span>
         <span class="op">${el.teachers[0].name}</span>
     </div>
+    <div class="rightside">
+    </div>
     <div class="side">
+    <span class="ruum">${el.rooms[0].roomCode}</span>
     </div>`
             timetable.appendChild(newLesson)
         }
